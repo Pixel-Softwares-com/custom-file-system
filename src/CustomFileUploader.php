22b2aa84d76da3fb40a9a4103df84d8653477e2c
Muhammed ALDRUBI <MuhammedALDRUBI>
2025-07-13 03:53:27 +0300

    laravel 12 support

--- src/CustomFileUploader.php
+++ src/CustomFileUploader.php
@@ -7,13 +7,11 @@ use Illuminate\Http\Request;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
-use CustomFileSystem\Traits\FileFakingMethods;
 use CustomFileSystem\Helpers\Helpers;
 
 abstract class CustomFileUploader extends CustomFileHandler
 {
-    use FileFakingMethods;
-
+ 
     protected array $file_path_uploadedFile_pairs = [];
 
     /**
@@ -173,4 +171,36 @@ abstract class CustomFileUploader extends CustomFileHandler
         //No matter if the $file_path_uploadedFile_pairs is empty or not .... if the execution come to this line the function get successful
         return $this->restartUploader();
     }
+
+    /**
+     * @param string $fileKey
+     * @param string $folderPath
+     * @return string
+     * @throws Exception
+     * return fake file's path and set it ready to uploading later when use uploadFiles Method
+     */
+    public function fakeSingleFile(string $fileKey , string $folderPath) : string
+    {
+        $files[$fileKey] = UploadedFile::fake()->image($fileKey);
+        return $this->processFile($files ,$fileKey , $folderPath )[$fileKey];
+    }
+
+    /**
+     * @param string $filesKey
+     * @param string $folderPath
+     * @param int $length
+     * @return array
+     * @throws Exception
+     */
+    public function fakeMultiFiles(string $filesKey , string $folderPath , int $length = 2) : array
+    {
+        $files = [];
+        for ($i=1;$i<=$length;$i++)
+        {
+            $files[$filesKey][] = UploadedFile::fake()->image($filesKey . "_" . $i);
+        }
+        return $this->processMultiUploadedFile($files , $filesKey , $folderPath )[$filesKey];
+    }
+
+
 }
--- src/Traits/FileFakingMethods.php
+++ src/Traits/FileFakingMethods.php
@@ -7,34 +7,4 @@ use Exception;
 
 trait FileFakingMethods
 {
-    /**
-     * @param string $fileKey
-     * @param string $folderPath
-     * @return string
-     * @throws Exception
-     * return fake file's path and set it ready to uploading later when use uploadFiles Method
-     */
-    public function fakeSingleFile(string $fileKey , string $folderPath) : string
-    {
-        $files[$fileKey] = UploadedFile::fake()->image($fileKey);
-        return $this->processFile($files ,$fileKey , $folderPath )[$fileKey];
     }
-
-    /**
-     * @param string $filesKey
-     * @param string $folderPath
-     * @param int $length
-     * @return array
-     * @throws Exception
-     */
-    public function fakeMultiFiles(string $filesKey , string $folderPath , int $length = 2) : array
-    {
-        $files = [];
-        for ($i=1;$i<=$length;$i++)
-        {
-            $files[$filesKey][] = UploadedFile::fake()->image($filesKey . "_" . $i);
-        }
-        return $this->processMultiUploadedFile($files , $filesKey , $folderPath )[$filesKey];
-    }
-
-}
